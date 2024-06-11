19b8ab7c9fc560200734c80307daeed5c099e7d9
WillEtheridge <will@originthread.com>
2024-06-11 09:11:05 +0100

    Added help function

--- timecalc.c
+++ timecalc.c
@@ -7,9 +7,17 @@ int isValidDecimalFormat(const char *str);
 
 int main( int argc, char *argv[])
 {   
+    // Check if there is an argument
     if (argc < 2) {
-        printf("Please enter the numbers you want to calculate in the form h.mm");
+        printf("Please enter the numbers you want to calculate in the form h.mm - Please run 'timecalc help' for help ");
         return 1;
+    }
+
+    // Print help text
+    if (strcmp( argv[1], "help") == 0)
+    {
+        printf("\nThis function is built to help you add up times.\n\nTo run it successfully, enter a comannd in the form 'timecalc 1.22 3.22 4.11' \n\nThere should be a single space between each time. Each time should be in the form h.mm \n\nIf you are just adding a minute value be sure to do so in the form 0.15 \n\nIf there are no minutes be sure to use the form 1.00\n\n");
+        return 2;
     }
 
     int hours = 0;
@@ -17,8 +25,8 @@ int main( int argc, char *argv[])
 
     for (int i = 1; i < argc; i++) {
         if (!isValidDecimalFormat(argv[i])) {
-            printf("Error: Time %d ('%s') is not in a valid format. Please enter all values in the form h.mm\n", i, argv[i]);
-            return 2; // Skip this iteration and move to the next argument
+            printf("Error: Time %d ('%s') is not in a valid format. Please enter all values in the form h.mm Please run 'timecalc help' for help\n", i, argv[i]);
+            return 3; // Skip this iteration and move to the next argument
         }
 
         char *input = argv[i];
